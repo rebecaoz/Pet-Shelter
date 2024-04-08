ac612666ac3f050218150ae159ea04c55469ae2f
Rebeca <lrpmqrp@gmail.com>
2024-04-08 20:27:55 -0300

    change axios post in main page

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-//const cors = require('cors')
+const cors = require('cors')
 const app = express();
 require('./config/mongoose.config')
-//app.use(cors())
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
